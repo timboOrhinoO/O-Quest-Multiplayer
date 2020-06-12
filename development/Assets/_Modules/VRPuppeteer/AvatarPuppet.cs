da6b297b6f90995710d221a45c4b2baa230ff235
reinemann <reinemann@prefrontalcortex.de>
2020-06-12 14:17:00 +0200

    [Puppets] alternative puppeteer approach

---
+++ development/Assets/_Modules/VRPuppeteer/AvatarPuppet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class AvatarPuppet : MonoBehaviourPun
+{
+    public Transform head, handL, handR;
+
+    void Start()
+    {
+        PhotonView view = GetComponent<PhotonView>();
+        if(view.IsMine){
+            LocalController local = FindObjectOfType<LocalController>();
+            local.remoteHead = head;
+            local.remoteHandL = handL;
+            local.remoteHandR = handR;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ development/Assets/_Modules/VRPuppeteer/LocalController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalController : MonoBehaviour
+{
+    public Transform localHead, remoteHead;
+    public Transform localHandR, remoteHandR;
+    public Transform localHandL, remoteHandL;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (localHead && remoteHead) 
+            CopyTransform(localHead, remoteHead);
+        if (localHandR && remoteHandR) 
+            CopyTransform(localHandR, remoteHandR);
+        if (localHandL && remoteHandL) 
+            CopyTransform(localHandL, remoteHandL);
+    }
+
+    void CopyTransform(Transform source, Transform target){
+        target.position = source.position;
+        target.rotation = source.rotation;
+    }
+}
