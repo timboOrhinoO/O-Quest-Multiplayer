c5d61233c6cb9bbb2c7f7eec1a47cd66005925eb
Tim Schuster <timaeschuster@gmail.com>
2020-06-21 23:53:07 +0200

    _ volspot property naming; crystals smoothness over noise changing; added material property block scripts; added particles to final scene;

---
+++ development/Assets/_Scripts/SetGlobalMaterialProperty.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SetGlobalMaterialProperty : MonoBehaviour
+{
+    [Header("Vector3")]
+    public string posPropertyName;
+    public Vector3 posValue;
+
+    [Header("Float")]
+    public string WaveDistName;
+    public float WaveDistance;
+
+    public string WaveOpacityName;
+    public float WaveOpacityValue;
+
+    [Header("Color")]
+    public string LightColorProperty;
+    public Color LightColor;
+    
+
+    //MaterialPropertyBlock block;
+    //public Renderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //block = new MaterialPropertyBlock();
+        //renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (!renderer) return;
+
+        posValue = transform.position;
+        if (posPropertyName != "") Shader.SetGlobalVector(posPropertyName, posValue);
+
+        if (WaveDistName != "") Shader.SetGlobalFloat(WaveDistName, WaveDistance);
+
+        if (WaveOpacityName != "") Shader.SetGlobalFloat(WaveOpacityName, WaveOpacityValue);
+
+        if (LightColorProperty != "") Shader.SetGlobalColor(LightColorProperty, LightColor);
+
+        //if (posPropertyName != "") block.SetVector(posPropertyName, posValue);
+
+        //renderer.SetPropertyBlock(block);
+    }
+}
---
+++ development/Assets/_Scripts/SetMaterialProperty.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SetMaterialProperty : MonoBehaviour
+{
+
+    MaterialPropertyBlock block;
+    public Renderer myRenderer;
+
+    [Header("Vector4")]
+    public string V4Name;
+    public Vector4 V4Value;
+    public string V4Name2;
+    public Vector4 V4Value2;
+    public string V4Name3;
+    public Vector4 V4Value3;
+
+    [Header("Float")]
+    public string FloatName;
+    public float FloatValue;
+    public string FloatName2;
+    public float FloatValue2;
+    public string FloatName3;
+    public float FloatValue3;
+    public string FloatName4;
+    public float FloatValue4;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (block == null) block = new MaterialPropertyBlock();
+        if (!myRenderer) myRenderer = GetComponent<Renderer>();
+
+        if (FloatName != "") block.SetFloat(FloatName, FloatValue);
+        if (FloatName != "") block.SetFloat(FloatName2, FloatValue2);
+        if (FloatName != "") block.SetFloat(FloatName3, FloatValue3);
+        if (FloatName != "") block.SetFloat(FloatName4, FloatValue4);
+
+        if (V4Name != "") block.SetVector(V4Name, V4Value);
+        if (V4Name != "") block.SetVector(V4Name2, V4Value2);
+        if (V4Name != "") block.SetVector(V4Name3, V4Value3);
+
+        if (myRenderer) myRenderer.SetPropertyBlock(block);
+    }
+}
